a25e7482754f0e91f898f3f60f3bedb811905764
Zayd Khan <zkhan03@villanova.edu>
2026-06-30 15:49:51 -0400

    fix bug

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { Analytics } from "@vercel/analytics/react";
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />

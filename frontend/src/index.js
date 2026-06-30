18bb517021ad5fc5f68389a8d44b83f4552d7367
Zayd Khan <zkhan03@villanova.edu>
2026-06-30 15:43:49 -0400

    uninstall speed insights

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/react"
 
 root.render(
   <React.StrictMode>
     <App />
     <Analytics />
-    <SpeedInsights />
   </React.StrictMode>
 );
--- node_modules/@vercel/speed-insights/dist/astro/component.ts
+++
@@ -1,2 +0,0 @@
-// @ts-expect-error typescript doesn't handle ./index.astro properly, but it's needed to generate types
-export { default } from './index.astro';

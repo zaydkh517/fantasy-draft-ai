06aea45e751f6a4930a935973fea74ebb736390c
Zayd Khan <zkhan03@villanova.edu>
2026-06-30 15:34:35 -0400

    import speed insights

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
     <Analytics />
+    <SpeedInsights />
   </React.StrictMode>
 );
